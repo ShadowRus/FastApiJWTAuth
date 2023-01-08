c32020c4d722da4154bd0c8733a244e00aecf78d
shadow-net <dimitriy.rusakov@gmail.com>
2023-01-08 13:57:31 +0300

    0.0.2 Start with JWT Auth: FastAPI and Streamlit

--- client.py
+++ client.py
@@ -12,13 +12,16 @@ def extract_ip():
         st.close()
     return IP
 
+
 def check_password():
+    """Returns `True` if the user had a correct password."""
+
     def password_entered():
         """Checks whether a password entered by the user is correct."""
         if (
-                st.session_state["username"] in st.secrets["passwords"]
-                and st.session_state["password"]
-                == st.secrets["passwords"][st.session_state["username"]]
+            st.session_state["username"] in st.secrets["passwords"]
+            and st.session_state["password"]
+            == st.secrets["passwords"][st.session_state["username"]]
         ):
             st.session_state["password_correct"] = True
             del st.session_state["password"]  # don't store username + password
@@ -26,14 +29,15 @@ def check_password():
         else:
             st.session_state["password_correct"] = False
 
-
-    if 'password_correct' not in st.session_state:
+    if "password_correct" not in st.session_state:
+        # First run, show inputs for username + password.
         st.text_input("Username", on_change=password_entered, key="username")
         st.text_input(
             "Password", type="password", on_change=password_entered, key="password"
         )
         return False
-    elif not st.session_state['password_correct']:
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
         st.text_input("Username", on_change=password_entered, key="username")
         st.text_input(
             "Password", type="password", on_change=password_entered, key="password"
@@ -41,28 +45,63 @@ def check_password():
         st.error("😕 User not known or password incorrect")
         return False
     else:
+        # Password correct.
+        return True
+
+
+
+
+
+
+
+def check_password():
+    def password_entered():
+        data = {
+            "username": str(username),
+            "password": str(password)
+        }
+        if "access_token" not in st.session_state:
+            r1 = requests.post(str(url + '/login'), json=data)
+            if r1.status_code == 200:
+                j1 = r1.json()
+                st.session_state['access_token'] = j1["access_token"]
+                st.session_state['refresh_token'] = j1['refresh_token']
+                st.session_state["password_correct"] = True
+            else:
+                st.session_state["password_correct"] = False
+        if ("access_token" in st.session_state and st.session_state["password_correct"] == False and 'refresh_token' in st.session_state ):
+            payload = {'Authorization': str('Bearer ' + st.session_state['refresh_token'])}
+            r1 = requests.get(str(url + '/refresh_token'), headers=payload)
+            if r1.status_code == 200:
+                j1 = r1.json()
+                st.session_state['access_token'] = j1["access_token"]
+
+
+
+
+    if 'password_correct' not in st.session_state:
+        username = st.text_input("Username", key="username")
+        password = st.text_input(
+            "Password", type="password", key="password")
+        if st.button('Login'):
+            password_entered()
+        if st.session_state["password_correct"]:
+            return True
+        else:
+            return False
+    else:
         return True
 
 HOST = extract_ip()
 PORT = 8084
 url ='http://'+ HOST+ ':' + str(PORT)
+if check_password():
+    st.write('gcvdh')
 
-with st.form('Auth'):
-    username = st.text_input('Login')
-    password = st.text_input('Password', type= 'password')
-    data = {
-        "username": str(username),
-        "password": str(password)
-    }
-    if st.form_submit_button('Login'):
-        r1 = requests.post(str(url + '/login'),json = data)
-        if r1.status_code == 200:
-            j1 = r1.json()
-            st.write(j1["access_token"])
-            st.session_state['access_token']= j1["access_token"]
 
-st.write('Get secrets')
-if st.button('Get'):
-    payload = {'Authorization': str('Bearer ' + st.session_state['access_token'])}
-    r1 = requests.post(str(url + '/secret'),headers= payload)
-    st.write(r1.text)
+
+    st.write('Get secrets')
+    if st.button('Get'):
+        payload = {'Authorization': str('Bearer ' + st.session_state['access_token'])}
+        r1 = requests.post(str(url + '/secret'),headers= payload)
+        st.write(r1.text)
